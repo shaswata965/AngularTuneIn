5b48a43e803edbd0944e1df4d959baf077e83d4f
shaswata965 <shaswata965@gmail.com>
2021-09-27 23:20:37 +0530

    Javascript Update

--- src/app/frontend/album-filter/album-filter.component.ts
+++ src/app/frontend/album-filter/album-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-album-filter',
@@ -10,6 +11,23 @@ export class AlbumFilterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  previousClicker(){
+    $("#previous").on("click",function(){
+      setTimeout(function (){
+        $("#firstYears").toggleClass("visible");
+        $("#secondYears").toggleClass("invisible");},300);
+    });
+  }
+
+  nextClicker(){
+    $("#next").on("click",function(){
+      setTimeout(function (){
+        $("#secondYears").toggleClass("invisible");
+        $("#firstYears").toggleClass("visible");
+      },300);
+    })
   }
 
 }
