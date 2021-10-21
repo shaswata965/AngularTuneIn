5d5321f686c55c4684ab0adbfc01e9896a7c9b1e
shaswata965 <shaswata965@gmail.com>
2021-10-22 02:23:52 +0530

    Final Update Before DataBase Client

--- src/app/frontend/player-component/player-component.component.ts
+++ src/app/frontend/player-component/player-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-player-component',
@@ -11,5 +12,34 @@ export class PlayerComponentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  shuffleOver(){
+     $('.playlist-tags').addClass('playlist-tags-display-visible');
+  }
+  shuffleOut(){
+    $('.playlist-tags').removeClass('playlist-tags-display-visible');
+  }
+  previousOver(){
+    $('.playlist-tags-previous').addClass('playlist-tags-display-visible');
+  }
+  previousOut(){
+    $('.playlist-tags-previous').removeClass('playlist-tags-display-visible');
+  }
+  playOver(){
+    $('.playlist-tags-play').addClass('playlist-tags-display-visible');
+  }
+  playOut(){
+    $('.playlist-tags-play').removeClass('playlist-tags-display-visible');
+  }
+  nextOver(){
+    $('.playlist-tags-next').addClass('playlist-tags-display-visible');
+  }
+  nextOut(){
+    $('.playlist-tags-next').removeClass('playlist-tags-display-visible');
+  }
+  repeatOver(){
+    $('.playlist-tags-repeat').addClass('playlist-tags-display-visible');
+  }
+  repeatOut(){
+    $('.playlist-tags-repeat').removeClass('playlist-tags-display-visible');
+  }
 }
